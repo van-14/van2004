136ec0166618ea31e199fff9c82e4d7136d9eed3
van-14 <vanpinkk4@gmail.com>
2024-08-21 16:01:45 +0700

    first commit

---
+++ thuchanhphp.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+</head>
+<body>
+    <?php
+    echo '<p style="text-align: center; color: green;">XIN CHAO</p>';
+    echo '<button style="background-color:red;>click</button>';
+    echo '<a target="_blank" href="https://www.youtube.com/">Youtube</a>'
+    ?>
+</body>
+</html>
